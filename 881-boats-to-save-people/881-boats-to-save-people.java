353ff303116e81d814a64dbcc04f205f5ae28cfa
wongruiyang <97069075+wongruiyang@users.noreply.github.com>
2022-03-24 11:36:19 +0800

    Time: 19 ms (78.29%), Space: 65.1 MB (64.40%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int solution = 0;
+        int start = 0;
+        int end = people.length -1;
+        
+        while(start <= end) {
+            solution++;
+            if(start == end) break; // last person on boat
+            if(people[start] + people[end] <= limit) start++; // we can carry two people
+            end--;
+        }
+        return solution;
+
+    }
+}
